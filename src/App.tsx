cc243ed99758b95b5a4cca60d83e222ce3eef11f
Kyle <kyle@Kyles-MacBook-Pro.local>
2024-01-08 12:06:50 +0000

    removed files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Components/Login';
-import SecondStep from './Components/AccountDetailsStep';
-import { Form } from './Components/Form';
+import React from "react";
+import "./App.css";
+import { Form } from "./Components/Form";
 
 function App() {
-  const handleFormSubmit = (loginData: any) => {
-    // Handle the form submit logic here
-    console.log(loginData);
-  };
-
   return (
     <div className="App">
-      <Login onFormSubmit={handleFormSubmit} />
-      {/* <Form /> */}
+      <Form />
     </div>
   );
 }
